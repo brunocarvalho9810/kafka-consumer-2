2bd7ae8f262c1606c9779950c1036dffaab61b08
Bruno Carvalho <brunocarvalho9810@gmail.com>
2023-03-29 22:08:19 -0300

    Adicionando headeres

--- src/main/java/com/kafka/consumer2/listener/TestListener.java
+++ src/main/java/com/kafka/consumer2/listener/TestListener.java
@@ -4,6 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.listener.adapter.ConsumerRecordMetadata;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
 
 /**
@@ -21,7 +24,16 @@ public class TestListener {
 	 * @author <a href="https://github.com/brunocarvalho9810/"> Bruno Carvalho </a>
 	 */
 	@KafkaListener(topics = "topic-1", groupId = "group-2")
-	public void listen(String message) {
-		logger.info("Thread: {} Messages: {}", Thread.currentThread().getId(), message);
+	public void listen(String message,
+					   @Header(KafkaHeaders.RECEIVED_TOPIC) String topic,
+					   @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition,
+					   ConsumerRecordMetadata metadata) { // ConsumerRecordMetadata utilizado apartir da versao 2.5
+		logger.info("Thread: {} Messages: {} Topico: {} Particao: {} Particao Metadata: {} Offset: {} ",
+				Thread.currentThread().getId(),
+				message,
+				topic,
+				partition,
+				metadata.partition(),
+				metadata.offset());
 	}
 }
